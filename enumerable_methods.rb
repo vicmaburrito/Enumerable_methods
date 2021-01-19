4623bd12804d90983bc357e87d4182f0db5a88c6
diamond-nicholas <diamondnicholas154@gmail.com>
2021-01-19 15:57:12 +0100

    add my_any? enumerables

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -11,6 +11,7 @@ module Enumerable
     end
     self
   end
+ 
   #my_each_with_index
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
@@ -30,15 +31,27 @@ module Enumerable
 
     my_each { |i| result << i if yield(i) }
     result
-   end
  end
  #my_all
  def my_all?
   if block_given?
+
    my_each {|result| return false unless yield result}
   else
-   my_each { |result| return false unless result }
-   end
-   true
-
+   my_each {|result| return false unless result }
   end
+    true
+ end
+ #my_any?
+ def my_any?
+  if block_given?
+
+   my_each {|result| return false unless yield result}
+  else
+   my_each {|result| return false unless result }
+  end
+   true
+ end
+
+end
+p %w[ant bear cat].my_any? { |word| word.length >= 3 }
