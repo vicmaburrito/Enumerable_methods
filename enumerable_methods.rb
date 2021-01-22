771c540fe8db7d41e9aa9c373eccf872c2af1aa2
diamond-nicholas <diamondnicholas154@gmail.com>
2021-01-22 09:25:24 +0100

    fix the tse required changes

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,6 +1,5 @@
 # rubocop:disable Metrics/CyclomaticComplexity
 # rubocop:disable Metrics/PerceivedComplexity
-# rubocop:disable Metrics/ModuleLength
 
 module Enumerable
   # my each
@@ -38,57 +37,38 @@ module Enumerable
     result
   end
 
-  # my_all
-  def my_all?(arg = nil)
-    if block_given?
-      my_each { |result| return false unless yield result }
-    elsif arg.is_a? Regexp
-      my_each { |result| return false if result.to_s =~ arg }
-    elsif arg.is_a? Class
-      my_each { |result| return false if result.is_a? arg }
-    else
-      my_each { |result| return false unless result }
-    end
+  def my_all?(*arg)
+    return grep(arg.first).length == size unless arg.empty?
+
+    my_each { |result| return false unless yield(result) } if block_given?
+    my_each { |result| return false unless result } unless block_given?
     true
   end
 
   # my_any?
-  def my_any?(arg = nil)
-    if block_given?
-      my_each { |result| return true unless yield result }
-    elsif arg.is_a? Regexp
-      my_each { |result| return true if result.to_s =~ arg }
-    elsif arg.is_a? Class
-      my_each { |result| return true if result.is_a? arg }
-    else
-      my_each { |result| return true unless result }
-    end
+  def my_any?(*arg)
+    return !grep(arg.first).empty? unless arg.empty?
+
+    my_each { |result| return true if yield(result) } if block_given?
+    my_each { |result| return true if result } unless block_given?
     false
   end
 
   # my_none
-  def my_none?(arg = nil)
-    if block_given?
-      my_each { |result| return false unless yield result }
-    elsif arg.is_a? Regexp
-      my_each { |result| return false if result.to_s =~ arg }
-    elsif arg.is_a? Class
-      my_each { |result| return false if result.is_a? arg }
-    else
-      my_each { |result| return false unless result }
-    end
+  def my_none?(*arg)
+    return grep(arg.first).empty? unless arg.empty?
+
+    my_each { |result| return false if yield(result) } if block_given?
+    my_each { |result| return false if result } unless block_given?
     true
   end
 
   # my_count
-  def my_count
-    return to_enum(:my_count) unless block_given?
+  def my_count(*arg, &block)
+    return my_select { |result| result == arg.first }.length unless arg.empty?
+    return my_select(&block).length if block_given?
 
-    count = 0
-    my_each do |i|
-      count += 1
-      yield(i)
-    end
+    size
   end
 
   # my_map
@@ -126,10 +106,16 @@ module Enumerable
     accumulator
   end
 
-  def multiply_els
-    my_inject(1, :*)
+  def multiply_els(arr)
+    arr.inject { |total, n| total * n }
   end
 end
+
+# p %w[ant bear cat].my_all? { |word| word.length >= 3 }
+# p %w[ant bear cat].my_all? { |word| word.length >= 4 }
+# p %w[ant bear cat].my_all?(/t/)
+# p [1, 2i, 3.14].my_all?(Numeric)
+# p [nil, true, 99].my_all?
+# p [].all?
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/PerceivedComplexity
-# rubocop:enable Metrics/ModuleLength
