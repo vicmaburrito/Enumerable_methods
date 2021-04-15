2ef018d14989cc92a8dd7f32ceac0c9a6521d672
vicmaburrito <vmaldaraca@gmail.com>
2021-04-15 15:22:32 -0500

    adding my_each_with_index method

--- spec/spec_enumerables.rb
+++ spec/spec_enumerables.rb
@@ -9,4 +9,13 @@ describe Enumerable do
       expect([1, 2, 3].my_each { |ele| }).to eql([1, 2, 3])
     end
   end
+
+  describe 'my_each_with_index' do
+    it 'returns to_enum if no arguments or block were given' do
+      expect([1, 2, 3].my_each_with_index).to be_a Enumerator
+    end
+    it 'returns an array with its index if block were given' do
+      expect([1, 2, 3].my_each_with_index { |ele, idx| }).to eql([1, 2, 3])
+    end
+  end
 end
