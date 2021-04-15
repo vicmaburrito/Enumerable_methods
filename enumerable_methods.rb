805cb11e286e838689082b0dd6e3bcb54e3c826e
vicmaburrito <vmaldaraca@gmail.com>
2021-04-15 15:10:22 -0500

    adding my_each method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,5 +1,4 @@
 module Enumerable
-  # my each
   def my_each
     return to_enum(:my_each) unless block_given?
 
@@ -12,7 +11,6 @@ module Enumerable
     self
   end
 
-  # my_each_with_index
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
@@ -25,7 +23,6 @@ module Enumerable
     self
   end
 
-  # my_select
   def my_select
     return to_enum(:my_select) unless block_given?
 
@@ -42,7 +39,6 @@ module Enumerable
     true
   end
 
-  # my_any?
   def my_any?(*arg)
     return !grep(arg.first).empty? unless arg.empty?
 
@@ -51,7 +47,6 @@ module Enumerable
     false
   end
 
-  # my_none
   def my_none?(*arg)
     return grep(arg.first).empty? unless arg.empty?
 
@@ -60,7 +55,6 @@ module Enumerable
     true
   end
 
-  # my_count
   def my_count(*arg, &block)
     return my_select { |result| result == arg.first }.length unless arg.empty?
     return my_select(&block).length if block_given?
@@ -68,7 +62,6 @@ module Enumerable
     size
   end
 
-  # my_map
   def my_map(par_ = nil)
     new_arr = []
     return to_enum unless block_given?
@@ -81,7 +74,6 @@ module Enumerable
     new_arr
   end
 
-  # my_inject
   def my_inject(*arg)
     res = nil
     if arg.first && [Integer, Float].include?(arg.first.class)
@@ -105,13 +97,3 @@ module Enumerable
     arr.inject { |total, n| total * n }
   end
 end
-
-# p [1,2,3,4].my_each {|n| p n + 2}
-# p [1,2,3,4].each {|n| p n + 2}
-
-# p %w[ant bear cat].my_all? { |word| word.length >= 3 }
-# p %w[ant bear cat].my_all? { |word| word.length >= 4 }
-# p %w[ant bear cat].my_all?(/t/)
-# p [1, 2i, 3.14].my_all?(Numeric)
-# p [nil, true, 99].my_all?
-# p [].all?
